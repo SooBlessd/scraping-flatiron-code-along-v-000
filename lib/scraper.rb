ff737056406f78673725552b86620efda5a02a0f
Valencia Caldwell <vkcaldwell@gmail.com>
2021-03-17 21:31:04 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,15 +16,15 @@ scraper.new.get_page
 
 
 
-  # def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course|
-  #     if course.title && course.title != ""
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-  #   end
-  # end
-
-end
+#   # def print_courses
+#   #   self.make_courses
+#   #   Course.all.each do |course|
+#   #     if course.title && course.title != ""
+#   #       puts "Title: #{course.title}"
+#   #       puts "  Schedule: #{course.schedule}"
+#   #       puts "  Description: #{course.description}"
+#   #     end
+#   #   end
+#   # end
+# 
+# end
