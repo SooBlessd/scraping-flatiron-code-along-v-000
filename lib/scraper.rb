164a781a05c170ade3615de258a44e7a7fdb7a12
Valencia Caldwell <vkcaldwell@gmail.com>
2021-03-17 21:57:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,9 @@ require_relative './course.rb'
 class Scraper
 
 
-  def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  end
+  # def get_page
+  #   doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  # end
 #     binding.pry
 #   end
 # end
